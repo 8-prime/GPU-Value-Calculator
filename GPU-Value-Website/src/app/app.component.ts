68632730f30b1298aa9766e4529c3a13d0ad1f1b
Stefan Hoffmeister <hoffmeister.stefan99@gmail.com>
2022-12-16 01:09:32 +0100

    sort is there but fonky

--- GPU-Value-Website/src/app/app.component.ts
+++ GPU-Value-Website/src/app/app.component.ts
@@ -11,6 +11,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   
   gpus?: GPU[];
+  sortedGpus?: GPU[];
 
 
   getGpusFromJson(data: any): GPU[] {
@@ -25,9 +26,60 @@ export class AppComponent {
 
     request.subscribe( data => {
       this.gpus = this.getGpusFromJson(data);
+      this.sortedGpus = this.gpus;
     } );
   }
 
+
+
+  brandSort: boolean = false;
+  performanceSort: boolean = false;
+  priceSort: boolean = false;
+  valueSort: boolean = false;
+
+
+  sortData(sortOn: string){
+    switch(sortOn){
+      case 'Brand':
+        this.sortedGpus = this.gpus?.sort((a, b) => compare(a.brand, b.brand, this.brandSort))
+        this.brandSort = !this.brandSort;
+        break;
+      case 'Performance':
+        if (this.selectedIndex === 0){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare(a.tenMedium, b.tenMedium, this.performanceSort))
+        }
+        if (this.selectedIndex === 1){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare(a.tenUltra, b.tenUltra, this.performanceSort))
+        }
+        if (this.selectedIndex === 2){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare(a.twoKUltra, b.twoKUltra, this.performanceSort))
+        }
+        if (this.selectedIndex === 3){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare(a.fourKUltra, b.fourKUltra, this.performanceSort))
+        }
+        this.performanceSort = !this.performanceSort;
+        break;
+      case 'Price':
+        this.sortedGpus = this.gpus?.sort((a, b) => compare(a.price, b.price, this.priceSort))
+        this.priceSort = !this.priceSort;
+        break;
+      case 'Value':
+        if (this.selectedIndex === 0){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare((a.tenMedium / a.price), (b.tenMedium / b.price), this.valueSort))
+        }
+        if (this.selectedIndex === 1){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare((a.tenUltra / a.price), (b.tenUltra / b.price), this.valueSort))
+        }
+        if (this.selectedIndex === 2){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare((a.twoKUltra / a.price), (b.twoKUltra / b.price), this.valueSort))
+        }
+        if (this.selectedIndex === 3){
+          this.sortedGpus = this.gpus?.sort((a, b) => compare((a.fourKUltra / a.price), (b.fourKUltra / b.price), this.valueSort))
+        }
+        this.valueSort = !this.valueSort;
+        break;
+    }
+  }
 
   constructor(private http: HttpClient) {
     this.getInitialData();
@@ -42,10 +94,19 @@ export class AppComponent {
     console.log(this.selectedIndex);    
   }
 
+
+
+
   settings : string[] = [
     "1080p Medium",
     "1080p Ultra",
     "1440p Ultra",
     "4K Ultra",
   ]
+}
+
+
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
